199724c215a1d83c8396dd30aedbfb793ede9c62
WODweqFS <prohnovigor11@gmail.com>
2022-07-04 17:03:36 +0300

    Some edits

--- src/main/java/com/prokhnov/projectcontent/aspect/ControllersAspect.java
+++ src/main/java/com/prokhnov/projectcontent/aspect/ControllersAspect.java
@@ -1,30 +1,41 @@
 package com.prokhnov.projectcontent.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Component
 @Aspect
 public class ControllersAspect {
 
+    private final String ARR = " >>> ";
+
     @Before("execution(public String save* (..))")
-    public void logBeforeSave(JoinPoint joinPoint){
-        System.out.println(joinPoint.getSignature().getName());
+    public void logBeforeSave(JoinPoint joinPoint) {
+        System.out.println(ARR + joinPoint.getSignature().getName()
+                + ARR + joinPoint.getSignature().getDeclaringTypeName());
     }
 
     @Before("execution(public String update* (..))")
-    public void logBeforeUpdate(JoinPoint joinPoint){
-        System.out.println(joinPoint.getSignature().getName());
+    public void logBeforeUpdate(JoinPoint joinPoint) {
+        System.out.println(ARR + joinPoint.getSignature().getName()
+                + ARR + joinPoint.getSignature().getDeclaringTypeName());
     }
 
     @Before("execution(public String delete* (..))")
-    public void logBeforeDelete(JoinPoint joinPoint){ System.out.println(joinPoint.getSignature().getName()); }
+    public void logBeforeDelete(JoinPoint joinPoint) {
+        System.out.println(ARR + joinPoint.getSignature().getName()
+                + ARR + joinPoint.getSignature().getDeclaringTypeName());
+    }
 
     @Before("execution(public String add* (..))")
-    public void logBeforeAdd(JoinPoint joinPoint) { System.out.println(joinPoint.getSignature().getName());
-
+    public void logBeforeAdd(JoinPoint joinPoint) {
+        System.out.println(ARR + joinPoint.getSignature().getName()
+                + ARR + joinPoint.getSignature().getDeclaringTypeName());
     }
 
 }
